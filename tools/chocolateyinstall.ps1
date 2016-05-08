a465709b3aeacca674c69554bf5ad9e0cc6d400b
David Gardiner <david@gardiner.net.au>
2016-05-08 21:04:37 +0930

    1.0 - Add TortoiseSVN

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -24,7 +24,44 @@ if (Test-Path HKCU:\SOFTWARE\TortoiseGit)
     $keySoftware = get-item HKCU:\SOFTWARE
 
     $r = $keySoftware.OpenSubKey("TortoiseGit", $true)
-    $r.SetValue("Diff", '"C:\Program Files (x86)\Beyond Compare 4\BComp.exe" %base %mine /title1=%bname /title2=%yname /leftreadonly')
+    $old = $r.GetValue("Diff")
+    if ($old -ne $null)
+    {
+        Write-Warning ("Old value - Diff: " + $old)
+    }
+    $r.SetValue("Diff", '"C:\Program Files (x86)\Beyond Compare 4\BComp.exe" %base %mine /title1=%bname /title2=%yname /leftreadonly', [Microsoft.Win32.RegistryValueKind]::String)
+
+    $old = $r.GetValue("Merge")
+    if ($old)
+    {
+        Write-Warning ("Old value - Merge: " + $old)
+    }
+    $r.SetValue("Merge", '"C:\Program Files (x86)\Beyond Compare 4\BComp.exe" %mine %theirs %base %merged /title1=%yname /title2=%tname /title3=%bname /title4=%mname', [Microsoft.Win32.RegistryValueKind]::String)
+    $r.Close()
+    $keySoftware.Close()
+}
+
+# TortoiseSVN
+if (Test-Path HKCU:\SOFTWARE\TortoiseSVN)
+{
+    Write-Verbose "Configuring TortoiseSVN"
+
+    $keySoftware = get-item HKCU:\SOFTWARE
+
+    $r = $keySoftware.OpenSubKey("TortoiseSVN", $true)
+
+    $old = $r.GetValue("Diff")
+    if ($old -ne $null)
+    {
+        Write-Warning ("Old value - Diff: " + $old)
+    }
+    $r.SetValue("Diff", '"C:\Program Files (x86)\Beyond Compare 4\BComp.exe" %base %mine /title1=%bname /title2=%yname /leftreadonly', [Microsoft.Win32.RegistryValueKind]::String)
+
+    $old = $r.GetValue("Merge")
+    if ($old)
+    {
+        Write-Warning ("Old value - Merge: " + $old)
+    }
     $r.SetValue("Merge", '"C:\Program Files (x86)\Beyond Compare 4\BComp.exe" %mine %theirs %base %merged /title1=%yname /title2=%tname /title3=%bname /title4=%mname', [Microsoft.Win32.RegistryValueKind]::String)
     $r.Close()
     $keySoftware.Close()
